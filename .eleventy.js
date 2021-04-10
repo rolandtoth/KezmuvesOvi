ffcb07738d1fee4598f3716e599154e92d98dde4
Roland Toth Pal <roland.toth@pwc.com>
2021-04-10 11:44:58 +0200

    add netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -3,84 +3,86 @@ TODO
 - image dimensions?
 - add images to Miért hozzánk? page
 */
-const htmlmin = require("html-minifier");
-const glob = require("glob");
-const cfg = require("./input/data/cfg.json");
+const htmlmin = require('html-minifier');
+const glob = require('glob');
+const cfg = require('./input/data/cfg.json');
 const path = require('path');
 
 module.exports = function (eleventyConfig) {
+    let out = {};
 
-  let out = {};
-
-  glob.sync("./filters/*.js").forEach(filePath => {
-    let pathInfo = path.parse(filePath);
-    eleventyConfig.addFilter(pathInfo.base.replace(pathInfo.ext, ""), require(filePath));
-  });
-
-  eleventyConfig.addPassthroughCopy("assets/");
-  eleventyConfig.addPassthroughCopy("_headers");
-  eleventyConfig.addPassthroughCopy("favicon.png");
-
-  eleventyConfig.addCollection("menu", function (collection) {
-    return collection.getFilteredByTag("menu").sort(function (a, b) {
-      return a.data.menuOrder - b.data.menuOrder;
+    glob.sync('./filters/*.js').forEach((filePath) => {
+        let pathInfo = path.parse(filePath);
+        eleventyConfig.addFilter(
+            pathInfo.base.replace(pathInfo.ext, ''),
+            require(filePath)
+        );
     });
-  });
 
-  eleventyConfig.addCollection("news", function (collection) {
-    return collection
-      .getFilteredByTag("news")
-      .filter(function (item) {
-        return (
-          item.data.date.substr(0, 4) >= new Date().getFullYear() - 1
-        );
-      })
-      .sort(function (a, b) {
-        return new Date(b.data.date) - new Date(a.data.date);
-      });
-  });
+    eleventyConfig.addPassthroughCopy('assets/');
+    eleventyConfig.addPassthroughCopy('_headers');
+    eleventyConfig.addPassthroughCopy('favicon.png');
+    eleventyConfig.addPassthroughCopy('admin');
+    eleventyConfig.addPassthroughCopy('static/img');
 
-  eleventyConfig.addCollection("galleries", function (collection) {
-    return collection
-      .getFilteredByTag("gallery")
-      .filter(function (item) {
-        return (
-          item.fileSlug.substr(0,1) !== '.'
-        );
-      })
-      .reverse();
-  });
+    eleventyConfig.addCollection('menu', function (collection) {
+        return collection.getFilteredByTag('menu').sort(function (a, b) {
+            return a.data.menuOrder - b.data.menuOrder;
+        });
+    });
 
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    if (outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: false
-      });
-      return minified;
+    eleventyConfig.addCollection('news', function (collection) {
+        return collection
+            .getFilteredByTag('news')
+            .filter(function (item) {
+                return (
+                    item.data.date.substr(0, 4) >= new Date().getFullYear() - 1
+                );
+            })
+            .sort(function (a, b) {
+                return new Date(b.data.date) - new Date(a.data.date);
+            });
+    });
+
+    eleventyConfig.addCollection('galleries', function (collection) {
+        return collection
+            .getFilteredByTag('gallery')
+            .filter(function (item) {
+                return item.fileSlug.substr(0, 1) !== '.';
+            })
+            .reverse();
+    });
+
+    eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+        if (outputPath.endsWith('.html')) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: false,
+            });
+            return minified;
+        }
+        return content;
+    });
+
+    out = {
+        dir: {
+            input: 'input',
+            data: 'data',
+            output: 'dist',
+            includes: 'includes',
+        },
+        templateFormats: ['njk', 'md'],
+        htmlTemplateEngine: 'njk',
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: false,
+        passthroughFileCopy: true,
+    };
+
+    if (cfg.pathPrefix) {
+        out.pathPrefix = cfg.pathPrefix;
     }
-    return content;
-  });
 
-  out = {
-    dir: {
-      input: "input",
-      data: "data",
-      output: "dist",
-      includes: "includes"
-    },
-    templateFormats: ["njk", "md"],
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
-    dataTemplateEngine: false,
-    passthroughFileCopy: true
-  }
-
-  if (cfg.pathPrefix) {
-    out.pathPrefix = cfg.pathPrefix;
-  }
-
-  return out;
+    return out;
 };
